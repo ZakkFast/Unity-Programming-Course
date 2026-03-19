e0a1e2e5235d62d1e5595c4ec332b5803d9c2778
ZakkFast <zakkfastpro@gmail.com>
2026-03-18 22:55:33 -0500

    fix spawning, destroy, and placement issues

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 0f;
+    private float spawnCoolDown = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= spawnCoolDown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0f;
+        }
+    }
+}
